d9148aefa820c55f732af50f6d04d24faa984ed0
niranjan1624 <niranjan.ece1@gmail.com>
2014-12-18 12:53:00 +0530

    Method for inserting Notification Details and to open the DB open, close methods are added

--- src/com/taramt/utils/DBAdapter.java
+++ src/com/taramt/utils/DBAdapter.java
@@ -3,32 +3,46 @@ package com.taramt.utils;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
+import java.util.ArrayList;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 public class DBAdapter {
-	
+
 	private final Context context;
 	private DatabaseHelper DBHelper;
 	private SQLiteDatabase db;
-	
-	//Constructor
-	public DBAdapter(Context c)
-	{
+	public DBAdapter(Context c) {
 		Log.d("AutoLog","Inside DBAdapter Constructor");
-		context=c;	
-		DBHelper=new DatabaseHelper(context);
-	}
-	
-	//Open database connection
-	public void open()
-	{
-		db = DBHelper.getWritableDatabase();
+		context = c;	
+		DBHelper = new DatabaseHelper(context);
+	}	
 
+
+	public long insertNotificationDetails(String packageName, 
+			String date, String time) {
+		Log.d("Notification",
+				"DBA: " + packageName 
+				+ " | " + date + " | " + time);
+		ContentValues cv = new ContentValues();
+		cv.put("appName", packageName);
+		cv.put("notificationDetails", date);
+		cv.put("timeStamp", time);
+		long n=db.insert("NotificationDetails", null, cv);
+		Log.d("NotificationDetails", "" + n);
+		return n;	
 	}
 
-	//close database connection
-	public void close()
-	{
+	public void open() {
+		db = DBHelper.getReadableDatabase();
+	}
+	public void close() {
 		DBHelper.close();
 	}
 
+	
 }
