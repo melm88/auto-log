1901c999290e23a4af74fa27ece028a877cdb0db
niranjan1624 <niranjan.ece1@gmail.com>
2014-12-18 15:06:12 +0530

    CreateNotification: To test the app for different functions

--- src/com/taramt/autolognotification/NotificationActivity.java
+++ src/com/taramt/autolognotification/NotificationActivity.java
@@ -1,7 +1,6 @@
 package com.taramt.autolognotification;
 
 import java.util.ArrayList;
-import android.support.v4.app.NotificationCompat;
 
 import android.app.Activity;
 import android.app.NotificationManager;
@@ -10,6 +9,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,6 +18,7 @@ import com.taramt.autolog.R;
 import com.taramt.utils.DBAdapter;
 public class NotificationActivity extends Activity {
 
+	private String TAG = this.getClass().getSimpleName();
 	private TextView txtView;
 	private NotificationReceiver nReceiver;
 	DBAdapter db;
@@ -32,7 +34,7 @@ public class NotificationActivity extends Activity {
 		filter.addAction("com.taramt.autolog.notification");
 		registerReceiver(nReceiver,filter);
 		db = new DBAdapter(this);
-		
+
 	}
 
 	@Override
@@ -40,11 +42,31 @@ public class NotificationActivity extends Activity {
 		super.onDestroy();
 		unregisterReceiver(nReceiver);
 	}
+	
+
+
+	public void CreateNotification(View v) {
+
+		if(v.getId() == R.id.btnCreateNotify){
+			NotificationManager nManager = (NotificationManager) 
+					getSystemService(NOTIFICATION_SERVICE);
+			NotificationCompat.Builder ncomp = 
+					new NotificationCompat.Builder(this);
+			ncomp.setContentTitle("My Notification");
+			ncomp.setContentText("Notification Listener Service");
+			ncomp.setTicker("Notification Listener Service Ticker");
+			ncomp.setSmallIcon(R.drawable.ic_launcher);
+			ncomp.setAutoCancel(true);
+			nManager.notify((int)System.currentTimeMillis(),ncomp.build());
+
+		}
+	}
 
 	class NotificationReceiver extends BroadcastReceiver {
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			
+			Log.d(TAG, "Notification received");	
+
 		}
 	}
 
