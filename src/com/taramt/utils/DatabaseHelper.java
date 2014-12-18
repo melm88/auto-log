41e2836281d5e2b6b424c56f5d8fb766c58b09c9
niranjan1624 <niranjan.ece1@gmail.com>
2014-12-18 12:48:55 +0530

    Database created for notification logging with TS

--- src/com/taramt/utils/DatabaseHelper.java
+++ src/com/taramt/utils/DatabaseHelper.java
@@ -1,28 +1,40 @@
 package com.taramt.utils;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-	
-	public DatabaseHelper(Context context) 
-	{
-		super(context,"AutoLog" , null, 1);
-		Log.d("AutoLog","Database created....");
+
+	public DatabaseHelper(Context context) {
+		super(context, "AutoLog" , null, 1);
+		Log.d("AutoLog", "Database created....");
 	}
 
 	//Create tables
-	public void onCreate(SQLiteDatabase db) 
-	{
-		//db.execSQL("create table if not exists email(mesgid INTEGER not null, subject text not null, email text not null, sender text not null, datetime text not null, receiver text not null, folder text not null, numofattachments integer not null, recipients text,  flags text not null, displayname text, attachmentName text, PRIMARY KEY(mesgid, email, folder));");
-		Log.d("AutoLog","Table Created!!!");
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("create table if not exists "
+				+ "NotificationDetails(appName text not null, "
+				+ "notificationDetails text not null,"
+				+ " timeStamp text not null);");
+		System.out.println();
+		Log.d("Notification", "Table Created!!!");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
+
+		try {
+
+			db.execSQL("DROP TABLE IF EXISTS NotificationDetails");
+			onCreate(db);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+
+		}
 
 	}
 }
