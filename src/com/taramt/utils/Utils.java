2468ed241b8d16d63880fc00ff3d4408e50b785c
niranjan1624 <niranjan.ece1@gmail.com>
2014-12-19 15:53:04 +0530

    A common method to display the database in the activity

--- src/com/taramt/utils/Utils.java
+++ src/com/taramt/utils/Utils.java
@@ -1,12 +1,16 @@
 package com.taramt.utils;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.widget.TextView;
 
 public class Utils {
 	Context context;
+	
 	public Utils(Context context) {
 		this.context = context;
 	}
@@ -24,6 +28,20 @@ public class Utils {
 				? packageManager.getApplicationLabel(applicationInfo) : "???");
 		return appName;
 	}
-
+	
+	/*
+	 * a method for showing the details from the database
+	 */
+	public void showDetails(DBAdapter db, ArrayList<String> details, TextView tv) {
+		db.open();
+		
+		details = db.getNotificationDetails();
+		String nDetailss = "";
+		for (int i = 0; i<details.size(); i++) {
+			nDetailss = details.get(i) + "\n\n" + nDetailss ;
+		}
+		tv.setText(nDetailss);
+		db.close();
+	}
 
 }
