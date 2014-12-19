59bec12e5ab280e698eb8f4f4d8beb06807abbf4
niranjan1624 <niranjan.ece1@gmail.com>
2014-12-19 16:12:38 +0530

    Alarmmanager: start and stop the DataService added to run every 5 minutes in the background

--- src/com/taramt/autologdatausage/DataUsage.java
+++ src/com/taramt/autologdatausage/DataUsage.java
@@ -1,39 +1,70 @@
 package com.taramt.autologdatausage;
 
+import java.util.ArrayList;
+
 import com.taramt.autolog.R;
 import com.taramt.autolog.R.id;
 import com.taramt.autolog.R.layout;
 import com.taramt.autolog.R.menu;
+import com.taramt.utils.DBAdapter;
+import com.taramt.utils.Utils;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.TextView;
 public class DataUsage extends Activity {
-
+	  SharedPreferences shared;
+	TextView dataUsage;
+	DBAdapter db;
+	Utils utils;
+	static ArrayList<String> dataUsageApps = new ArrayList<String>();
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_data_usage);
+		setContentView(R.layout.activity_main);
+		dataUsage  = (TextView) findViewById(R.id.dataUsage);
+		db = new DBAdapter(this);
+		utils = new Utils(this);
+		startAlarm();
+		Log.d("DATAUSAGE", "in on create");
 	}
+	 
+	public void startAlarm() {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.data_usage, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		try {
+			Intent intentt = new Intent(getApplicationContext(), DataService.class);
+			PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(),0,intentt,PendingIntent.FLAG_UPDATE_CURRENT);
+			AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+			am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),
+					1000*60*5, pendingIntent);
+		} catch (Exception e) {
+			Log.d("exceptionasdfdf",""+e);
 		}
-		return super.onOptionsItemSelected(item);
+		
 	}
+public void stopService() {
+	Intent intent = new Intent(this, DataService.class);
+	stopService(intent);
+	try {
+		
+		PendingIntent pendingIntent = PendingIntent.getService(this, 
+				0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		AlarmManager am =(AlarmManager)getSystemService(Context.ALARM_SERVICE);
+		am.cancel(pendingIntent);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+
+
 }
