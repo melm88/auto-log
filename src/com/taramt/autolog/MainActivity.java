88ae9f11eb88dea1578a7fa9b27433a616a1394f
Ashok Madudru <ashokgirihari@gmail.com>
2014-12-22 19:43:21 +0530

    button is added to main layout for opening call log layout. call log functionality is completed

--- src/com/taramt/autolog/MainActivity.java
+++ src/com/taramt/autolog/MainActivity.java
@@ -2,13 +2,25 @@ package com.taramt.autolog;
 
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
+	Context context;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		context=this;
+	}
+	
+	public void getCallLog(View view){
+		Log.d("main","calllog");
+		Intent intent =new Intent(context,Calllog.class);
+		startActivity(intent);
 	}
 }
