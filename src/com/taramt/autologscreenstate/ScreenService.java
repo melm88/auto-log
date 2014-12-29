01f763c02a266e61ced34f8dd78a866b96976062
niranjan1624 <niranjan.ece1@gmail.com>
2014-12-29 12:30:13 +0530

    changed some of the logics of inserting data to save data into database correctly

--- src/com/taramt/autologscreenstate/ScreenService.java
+++ src/com/taramt/autologscreenstate/ScreenService.java
@@ -1,7 +1,5 @@
 package com.taramt.autologscreenstate;
 
-import com.taramt.utils.Utils;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -9,15 +7,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.IBinder;
-import android.preference.PreferenceManager;
 import android.util.Log;
+
+import com.taramt.utils.Utils;
 
 public class ScreenService extends Service {
 
-	SharedPreferences prefs;
+
 	String alarm = "ALARM";
 	Utils utils;
 	static BroadcastReceiver mReceiver = new ScreenReceiver();
@@ -40,14 +37,13 @@ public class ScreenService extends Service {
 	}
 	@Override
 	public void onStart(Intent intent, int startId) {
-		prefs=PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 		filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
 		filter.addAction(Intent.ACTION_USER_PRESENT);
 		try {
 			unregisterReceiver(mReceiver);
 			Log.i("register oonService", "receiver unregistered");
 		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
+			
 		}
 		registerReceiver(mReceiver, filter);
 		Log.i("register oonService", "receiver registered");
@@ -56,6 +52,7 @@ public class ScreenService extends Service {
 	}
 
 	public void stopAlarm() {
+		
 		Intent intent = new Intent(getApplicationContext(), ScreenService.class);
 		PendingIntent pendingIntent = PendingIntent.getService(getApplicationContext(),0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
 		AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
