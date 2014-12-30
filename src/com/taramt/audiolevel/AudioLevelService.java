7c7b6a9857d770e2a220ec0d9f54ecd93c12bf2c
akhilesh s <akhilesh@taramt.com>
2014-12-30 16:00:25 +0530

    Background Audio Service Added

---
+++ src/com/taramt/audiolevel/AudioLevelService.java
@@ -0,0 +1,77 @@
+package com.taramt.audiolevel;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class AudioLevelService extends Service {
+	private static final int sampleRate = 8000;
+	private AudioRecord audio;
+	private int bufferSize;
+	private double lastLevel = 0;
+	@Override
+public IBinder onBind(Intent arg0) {
+return null;
+}
+@Override
+public int onStartCommand(Intent intent, int flags, int startId) {
+	Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+	try {
+		bufferSize = AudioRecord
+				.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO,
+						AudioFormat.ENCODING_PCM_16BIT);
+	} catch (Exception e) {
+		android.util.Log.e("TrackingFlow", "Exception", e);
+	}
+
+	audio = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate,
+			AudioFormat.CHANNEL_IN_MONO,
+			AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+
+	audio.startRecording();
+	readAudioBuffer();//After this call we can get the last value assigned to the lastLevel variable
+	audio.stop();
+	return START_STICKY;
+}
+
+/**
+ * Functionality that gets the sound level out of the sample
+ */
+private void readAudioBuffer() {
+
+	try {
+		short[] buffer = new short[bufferSize];
+
+		int bufferReadResult = 1;
+
+		if (audio != null) {
+
+			// Sense the voice...
+			bufferReadResult = audio.read(buffer, 0, bufferSize);
+			double sumLevel = 0;
+			for (int i = 0; i < bufferReadResult; i++) {
+				sumLevel += buffer[i];
+			}
+			lastLevel = Math.abs((sumLevel / bufferReadResult));
+		Log.d("Audio Level", lastLevel+"");
+		}
+
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+}
+
+
+@Override
+public void onDestroy() {
+super.onDestroy();
+
+Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+}
+
+}
--- src/com/taramt/audiolevel/MainActivity.java
+++ src/com/taramt/audiolevel/MainActivity.java
@@ -1,122 +1,77 @@
 package com.taramt.audiolevel;
 
+import java.util.ArrayList;
+
 import com.taramt.autolog.R;
+import com.taramt.utils.DBAdapter;
 
 import android.app.Activity;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaRecorder;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	private static final int sampleRate = 8000;
-	private AudioRecord audio;
-	private int bufferSize;
-	private double lastLevel = 0;
-	private Thread thread;
-	private static final int SAMPLE_DELAY = 75;
 	//private ImageView mouthImage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_audiolevel);
 		
 //		mouthImage = (ImageView)findViewById(R.id.mounthHolder);
 //		mouthImage.setKeepScreenOn(true);
 		Log.d("Activity", "onCreate");
-		try {
-			bufferSize = AudioRecord
-					.getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO,
-							AudioFormat.ENCODING_PCM_16BIT);
-		} catch (Exception e) {
-			android.util.Log.e("TrackingFlow", "Exception", e);
-		}
-		Log.d("Activity", "onCreate Ended");
+		
+		SharedPreferences savedValues = PreferenceManager
+				.getDefaultSharedPreferences(this);
+				//If this is the first launch
+				if (savedValues.getBoolean(getString(R.string.StartAudio), true)) {
+				Log.d("flag", "timer is being set");
+				SharedPreferences.Editor editor = savedValues.edit();
+				editor.putBoolean(getString(R.string.StartAudio), false);
+				editor.commit();
+				//Starting a AudioLevelService
+				Intent intent = new Intent(this, AudioLevelService.class);
+				PendingIntent pintent = PendingIntent
+				.getService(this, 0, intent, 0);
+				AlarmManager alarm = (AlarmManager)
+				getSystemService(Context.ALARM_SERVICE);
+				alarm.cancel(pintent);
+				//setting an alarm manager for interval of 5 minutes
+				alarm.setRepeating(AlarmManager.RTC_WAKEUP,
+				System.currentTimeMillis()+5*60*1000,
+				5*60*1000, pintent);
+				} else {
+				Log.d("flag", "timer is already set");
+				}
+				//displaying the log from database on text view
+				TextView tv = (TextView)findViewById(R.id.audiolevellog);
+				DBAdapter db = new DBAdapter(this);
+				db.open();
+				ArrayList<String> row = db.getLightSensorlog();
+				String content = "";
+				for (int i = 0; i < row.size(); i++) {
+				content = content + row.get(i)+ "\n\n";
+				}
+				db.close();
+				tv.setText(content);
+
 		
 	}
 
 	protected void onResume() {
 		super.onResume();
-		Log.d("Activity", "onResume Started...");
 		
-		audio = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate,
-				AudioFormat.CHANNEL_IN_MONO,
-				AudioFormat.ENCODING_PCM_16BIT, bufferSize);
-
-		audio.startRecording();
-		thread = new Thread(new Runnable() {
-			public void run() {
-				while(thread != null && !thread.isInterrupted()){
-					//Let's make the thread sleep for a the approximate sampling time
-					try{Thread.sleep(SAMPLE_DELAY);}catch(InterruptedException ie){ie.printStackTrace();}
-					readAudioBuffer();//After this call we can get the last value assigned to the lastLevel variable
-					
-					runOnUiThread(new Runnable() {
-						
-						@Override
-						public void run() {
-							if(lastLevel > 0 && lastLevel <= 50){
-								//mouthImage.setImageResource(R.drawable.mouth4);
-							}else
-							if(lastLevel > 50 && lastLevel <= 100){
-								//mouthImage.setImageResource(R.drawable.mouth3);
-							}else
-							if(lastLevel > 100 && lastLevel <= 170){
-								//mouthImage.setImageResource(R.drawable.mouth2);
-							}
-							if(lastLevel > 170){
-								//mouthImage.setImageResource(R.drawable.mouth1);
-							}
-						}
-					});
-				}
 			}
-		});
-		thread.start();
-	}
-	
-	/**
-	 * Functionality that gets the sound level out of the sample
-	 */
-	private void readAudioBuffer() {
-
-		try {
-			short[] buffer = new short[bufferSize];
-
-			int bufferReadResult = 1;
-
-			if (audio != null) {
-
-				// Sense the voice...
-				bufferReadResult = audio.read(buffer, 0, bufferSize);
-				double sumLevel = 0;
-				for (int i = 0; i < bufferReadResult; i++) {
-					sumLevel += buffer[i];
-				}
-				lastLevel = Math.abs((sumLevel / bufferReadResult));
-			Log.d("Audio Level", lastLevel+"");
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
+		@Override
 	protected void onPause() {
 		super.onPause();
-		thread.interrupt();
-		thread = null;
-		try {
-			if (audio != null) {
-				audio.stop();
-				audio.release();
-				audio = null;
-			}
-		} catch (Exception e) {e.printStackTrace();}
 	}
 }
