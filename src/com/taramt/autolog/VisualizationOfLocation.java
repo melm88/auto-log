938841322f91d09f6ae787eb691e1d4f3bfb1776
Ashok Madudru <ashokgirihari@gmail.com>
2015-01-02 16:20:14 +0530

    async task class is added to visualizationclass for getting data from server

--- src/com/taramt/autolog/VisualizationOfLocation.java
+++ src/com/taramt/autolog/VisualizationOfLocation.java
@@ -1,5 +1,21 @@
 package com.taramt.autolog;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -7,8 +23,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
 public class VisualizationOfLocation extends FragmentActivity {
 
@@ -54,3 +72,59 @@ public class VisualizationOfLocation extends FragmentActivity {
 	}
 
 }
+
+class LocationOnMap extends AsyncTask<String, Integer,String>{
+
+
+	@Override
+	protected String doInBackground(String... params) {
+		// TODO Auto-generated method stub
+
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost("https://autocode.pythonanywhere.com/AAT/webapi/getCord");
+		try {
+			// Add your data
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+			nameValuePairs.add(new BasicNameValuePair("email", "rrcmuedu@gmail.com"));
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+			HttpResponse response = httpclient.execute(httppost);
+
+			HttpEntity entity = response.getEntity();
+			if(entity != null)
+			{
+				VisualizationOfLocation.resp = EntityUtils.toString(entity);
+				Log.d("resp",VisualizationOfLocation.resp);
+
+				//return MainActivity.resp;
+			}
+
+
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+
+
+	}
+
+	//@Override
+	/*protected void onPostExecute(String result) {
+		// TODO Auto-generated method stub
+		super.onPostExecute(result);
+		 MapActivity.response=result;
+		 
+		 Intent myIntent=new Intent(MainActivity.context,MapActivity.class);
+		 startActivity(myIntent);
+		
+	}*/
+	
+
+}
+
