b4f1ac2b3ef27d87d9bc71b8565afff14007efe1
niranjan1624 <niranjan.ece1@gmail.com>
2015-01-03 11:23:46 +0530

    manifest and resource files added

--- gen/com/taramt/autolog/R.java
+++ gen/com/taramt/autolog/R.java
@@ -984,12 +984,6 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
-        public static final int Average=0x7f050044;
-        public static final int Datausage=0x7f05003c;
-        public static final int NotificationDetails=0x7f05003d;
-        public static final int Sort=0x7f050047;
-        public static final int Top3=0x7f050045;
-        public static final int Total_duration=0x7f050043;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1001,14 +995,13 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050048;
+        public static final int action_settings=0x7f05003d;
         public static final int activity_chooser_view_content=0x7f050025;
+        public static final int alarmDetails=0x7f05003c;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int btnCreateNotify=0x7f050041;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int dataUsage=0x7f050040;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1024,7 +1017,6 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int log=0x7f050046;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1032,8 +1024,6 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
-        public static final int screenActivity=0x7f05003e;
-        public static final int scrollView1=0x7f05003f;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1051,7 +1041,6 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView=0x7f050042;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1107,18 +1096,11 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_controller=0x7f030017;
-        public static final int activity_data_usage=0x7f030018;
-        public static final int activity_main=0x7f030019;
-        public static final int activity_notification=0x7f03001a;
-        public static final int activity_screen=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int activity_alarmactivity=0x7f030017;
+        public static final int support_simple_spinner_dropdown_item=0x7f030018;
     }
     public static final class menu {
-        public static final int controller=0x7f0c0000;
-        public static final int data_usage=0x7f0c0001;
-        public static final int notification=0x7f0c0002;
-        public static final int screen=0x7f0c0003;
+        public static final int alarmactivity=0x7f0c0000;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
