7890fcf6f7134725d4259d11d50f8ec1b4131cf5
melvinm88 <melvin.m@taramt.com>
2015-01-06 10:02:16 +0530

    Modified: MainActivity was cleaned. Removed all un-neccasary code.

--- src/com/taramt/autolog/MainActivity.java
+++ src/com/taramt/autolog/MainActivity.java
@@ -1,12 +1,6 @@
 package com.taramt.autolog;
 
-import java.util.ArrayList;
-
-import com.taramt.temperature.TemperatureSensor;
-import com.taramt.utils.DBAdapter;
-
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -20,51 +14,8 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		//TextView in MainActivity
-		tv = (TextView) findViewById(R.id.mainactTV);
-
-		enableTemperatureSensing();
+		tv = (TextView) findViewById(R.id.mainactTV);	
 		
-		//Intent it = new Intent(this, SensorActivity.class);
-		//startActivity(it);		
-		
-	}
-	
-	
-	@Override
-	protected void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
 	}
 
-	//Method to initiate the service for capturing AmbientTemperature
-	public void enableTemperatureSensing() {
-		Intent iServe = new Intent(MainActivity.this, TemperatureSensor.class);
-		MainActivity.this.startService(iServe);
-		displayTemperatureSensingData();
-	}
-	
-	//Method to stop the service for capturing AmbientTemperature
-	public void stopTemperatureSensing() {
-		Intent iServe = new Intent(MainActivity.this, TemperatureSensor.class);
-		MainActivity.this.stopService(iServe);
-	}
-	
-	//Method to display the results from Temperature table onto MainActivity
-	public void displayTemperatureSensingData() {
-		DBAdapter dba = new DBAdapter(this);
-		dba.open();
-		ArrayList<String> TemperatureData = dba.getTemperatureDetails();
-		String result = "";
-		
-		//Display results only if there are data in DB.
-		if(TemperatureData.size() > 0) {
-			for(String temper: TemperatureData) {
-				result += temper + "\n\n";
-			}
-			tv.setText(result);
-		} else {
-			tv.setText("No Temperature Changes recorded so far");
-		}
-		dba.close();
-	}
 }
